e019d14571d1f31bc7b6ea551cedb3f6a010fc86
RedSerperior <anemagou@asu.edu>
2025-08-29 18:03:37 -0700

    Aug 28 submission 1

---
+++ python_files/easy/2828CheckifaStringIsanAcronymofWords.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        a=""
+        for i in words:
+            a+=i[0]
+        if a==s:
+            return True
+        return False
