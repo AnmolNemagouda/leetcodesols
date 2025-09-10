ccf39d078fa6b1a404fc3664b1141566ddccd97d
RedSerperior <anemagou@asu.edu>
2025-09-10 14:38:07 -0700

    Sep 9 submission 2

---
+++ python_files/medium/2161PartitionArrayAccordingtoGivenPivot.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        a,b,c=[],[],[]
+        for i in nums:
+            if i<pivot:
+                a.append(i)
+            elif i==pivot:
+                b.append(i)
+            else:
+                c.append(i)
+        a.extend(b)
+        a.extend(c) 
+        del c
+        del b     
+
+        return a
+                
